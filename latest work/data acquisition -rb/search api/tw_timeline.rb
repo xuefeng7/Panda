157810d08414de19a0359e6ecc378feaf3143594
URDeVotee <Sonny@dhcp-10-5-17-158.wireless.rochester.edu>
2016-05-31 21:52:44 -0400

    storing data

--- latest work/data acquisition -rb/search api/tw_timeline.rb
+++ latest work/data acquisition -rb/search api/tw_timeline.rb
@@ -3,6 +3,7 @@ require 'json'
 require 'uri'
 require "open-uri"
 require "base64"
+require 'yaml'
 
 # Authen
 consumer_key = "yBH5FlKN8vU79EMWNJtJkjcvI"
@@ -30,25 +31,45 @@ $access_token = JSON.parse(res.body)['access_token']
 
 
 $tweets = File.open("timeline.txt", 'a+')
+$user_dic = YAML.load_file('user_dic.yml')
+# Initialization
+if $user_dic.class == FalseClass
+	$user_dic = Hash.new
+end
 
 # Search timeline by keyword
 def searchTimelineByKeyword()
-	url = "https://api.twitter.com/1.1/statuses/user_timeline.json?"
-	# User Id
-	url = url + "user_id=25666128"
-	uri = URI.parse(url + "&count=3200")
-	http = Net::HTTP.new(uri.host, uri.port)
-	http.use_ssl = true
-	#init header
-	req = Net::HTTP::Get.new(uri.request_uri, initheader = {
-		'Authorization' => "Bearer " + $access_token
-		})
-	res = http.request(req)
-	if res.code == 409 then
-		#no quota
-		exit 
+	counter = 0
+	users = File.open("twitter_tag=selfie.txt", 'r')
+	users.each_line do |line|
+		u_json = JSON.parse(line)
+		if not $user_dic.has_key? u_json["user"]
+			url = "https://api.twitter.com/1.1/statuses/user_timeline.json?"
+			# User Id
+			url = url + "user_id=" + u_json["user"]
+			uri = URI.parse(url + "&count=3200")
+			http = Net::HTTP.new(uri.host, uri.port)
+			http.use_ssl = true
+			#init header
+			req = Net::HTTP::Get.new(uri.request_uri, initheader = {
+				'Authorization' => "Bearer " + $access_token
+				})
+			res = http.request(req)
+			if res.code == 409 then
+				#no quota
+				exit 
+			end
+			processTweets(JSON.parse(res.body),u_json["user"])
+			puts "#{counter} users finished"
+			counter += 1
+		end
 	end
-	return JSON.parse(res.body)
+	users.close
+	puts "Search is done"
+	File.open('user_dic.yml','w') do |file|
+		file.write $user_dic.to_yaml
+	end
+	exit
 end
 
 def getTimelineByWindow()
@@ -57,33 +78,40 @@ def getTimelineByWindow()
 	reqCounter = 0
 	while true do
 	#puts "req left: #{reqCounter}"
-	data = searchTimelineByKeyword()
+	searchTimelineByKeyword()
 	puts "Search is done"
-	processTweets(data)
-	sleep 2
 	end
 end
 
 # Filter out selfie
-def processTweets(tweets)
-	counter = 0
+def processTweets(tweets,user_id)
+	url_array = Array.new
 	for tweet in tweets
 		selfie = false
-		if tweet["entities"].has_key? "hashtags" then
-			for tags in tweet["entities"]["hashtags"]
-				keyword = tags["text"].downcase
-				if keyword.include? "selfie" or keyword.include? "me" or keyword.include? "photooftheday" or keyword.include? "picoftheday" or keyword.include? "happy" or keyword.include? "fun" or keyword.include? "smile" or keyword.include? "summer" or keyword.include? "friends" or keyword.include? "fashion"
-					selfie = true
+		if tweet.class == Hash
+			if tweet.has_key? "entities" then
+				if tweet["entities"].has_key? "hashtags" then
+					for tags in tweet["entities"]["hashtags"]
+						keyword = tags["text"].downcase
+						if keyword.include? "selfie" or keyword.include? "me" or keyword.include? "photooftheday" or keyword.include? "picoftheday" or keyword.include? "happy" or keyword.include? "fun" or keyword.include? "smile" or keyword.include? "summer" or keyword.include? "friends" or keyword.include? "fashion"
+							selfie = true
+						end
+					end
 				end
 			end
 		end
-		if tweet["entities"].has_key? "media" and selfie == true
-			url = tweet["entities"]["media"][0]["media_url"]
-			$tweets.write(url)
-			$tweets.write("\n")
+		if tweet.class == Hash
+			if tweet.has_key? "entities" then
+				if tweet["entities"].has_key? "media" and selfie == true
+					url = tweet["entities"]["media"][0]["media_url"]
+					url_array << url
+				end
+			end
 		end
 	end
-	exit
+	if not url_array.empty?
+		$user_dic[user_id] = url_array
+	end
 end
 
 getTimelineByWindow()
